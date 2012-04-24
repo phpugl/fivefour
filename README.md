44253d81eef67ac72a971c8c2ccd20f5a625f466
Sebastian Schulze <github.com@bascht.com>
2012-04-24 22:41:57 +0200

    - few more readme lines + correct manifest deps

--- README.md
+++ README.md
@@ -2,12 +2,19 @@ PHPUGL 5.4
 ==========
 
 [PHPUGL](http://phpugl.de) demo box with PHP 5.4 from PPA.
-Please don't use it to build fresh boxes, as `/manifests/default´ is a complete mess.
+Please don't use it to build fresh boxes, as `/manifests/default` is a complete mess.
 Should work with Vagrant >= 0.9.7.
 
     gem install vagrant
     vagrant up
 
+Wait a few seconds. Reload if necessary, or if you where offline during bootup.
+(The box needs to be online to get fresh apt sources)
 
-A few reloads might be necessary (see above).
+Now point your browser to http://33.33.33.96/ or alias the IP address to **phpugl.local**.
 
+Features
+--------
+* nginx 0.7.65-1ubuntu2.2
+* php-cli 5.4.0-3~lucid+4
+* php-fpm 5.4.0-3~lucid+4
--- manifests/default.pp
+++ manifests/default.pp
@@ -9,23 +9,25 @@ class phpugl {
   package { "git-core": ensure => present }
   package { "php5-fpm": ensure => present,    require => Apt::Ppa["ppa:ondrej/php5"] }
   package { "php5-cli": ensure => present,    require => Apt::Ppa["ppa:ondrej/php5"] }
-  package { "nginx": ensure => present }
+  package { "nginx": ensure => present, require => Exec['apt-get update'] }
 
   file { "/etc/timezone": content => "Europe/Berlin" }
   file { "/etc/localtime": source => "/usr/share/zoneinfo/Europe/Berlin" }
   file { "/etc/ssh/sshd_config": source => "/vagrant/manifests/files/sshd_config" }
 
-  service { "nginx": ensure => running }
+  service { "nginx": ensure => running, require => Package["nginx"] }
 
-  file { "/etc/nginx/sites-enabled/default": ensure => absent }
+  file { "/etc/nginx/sites-enabled/default": ensure => absent, require => Package["nginx"] } 
   file { "/etc/nginx/sites-available/phpugl.local":
     ensure => link, 
     notify => Service["nginx"],
+    require => Package["nginx"],
     target => "/vagrant/manifests/files/phpugl.local"
   }
   file { "/etc/nginx/sites-enabled/phpugl.local":
     ensure => link, 
     notify => Service["nginx"],
+    require => Package["nginx"],
     target => "/etc/nginx/sites-available/phpugl.local"
   }
 
